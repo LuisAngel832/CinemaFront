1795da6e28fd4868b01754be677763d3638966d6
Luis Angel <luisgelipe.12340@gmail.com>
2025-11-12 11:53:50 -0600

    Feat: create project structure

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter,Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
+import  LoginPage  from './pages/LoginPage.jsx'
+import { Navbar } from './components/Navbar'
+import './App.css'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <Navbar />
+
+
+
+        <Routes>
+          <Route path='/' element={<LoginPage/>} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  )
+}
+
+export default App
